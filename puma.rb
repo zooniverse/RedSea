bbca279a564bc5de337080068c9e8c7eb261135f
Campbell Allen <campbell.allen@gmail.com>
2020-11-25 17:26:12 +0000

    add thread controller to puma
    only run 1 thread in dev by default

--- puma.rb
+++ puma.rb
@@ -3,4 +3,6 @@
 env = ENV.fetch('RACK_ENV', 'development')
 port = ENV.fetch('PORT', 80)
 environment env
+max_threads = ENV.fetch('MAX_THREADS', 2).to_i
+threads 1, max_threads
 bind "tcp://0.0.0.0:#{port}"
